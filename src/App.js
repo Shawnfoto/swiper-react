6df77391e65fddef2602fa01af16c48579525481
Shawnfoto <rpd11140@gmail.com>
2020-12-12 17:04:29 +0800

    add desc

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import no_profile_image from "./images/no_profile_image.jpg";
 // icon
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 
 import {data} from "./json"
 
@@ -41,61 +41,67 @@ function App() {
         return (
           <SwiperSlide key={`slide-${i}`} tag='li'>
             <div className="img-container">
-            <img
-            src={`${item.to.photoRect}`}
-            alt={`Slide ${i}`}
-            />
-            <div className="info">
-                <div className="name">{item.to.name}</div>
-                <div className="job-title">
-                  <BusinessCenterIcon className="icon"/>
-                  <div className="text">{item.to.jobTitle}</div>
-                </div>
-                <div className="location">
-                  <LocationOnIcon className="icon"/>
-                  <div className="text"> {item.to.displayLocation}</div>
-                </div>
-                <div className="backgrounds">
-                  {item.to.backgrounds.map((background, i) => {
-                    return (
-                      <Chip 
-                      key={`background-chip-${i}`}
-                      className="chip-space"
-                      label={background}
-                      color="primary"
-                      size="small"
-                       />
-                    )
-                  })}
+              <img
+              src={`${item.to.photoRect}`}
+              alt={`Slide ${i}`}
+              />
+              <div className="info">
+                  <div className="name info-padding">{item.to.name}</div>
+                  <div className="job-title info-padding">
+                    <BusinessCenterIcon className="icon"/>
+                    <div className="text">{item.to.jobTitle}</div>
                   </div>
-                <div className="industries">
-                {item.to.industries.map((industry, i) => {
-                    return (
-                      <Chip 
-                      key={`industry-chip-${i}`}
-                      className="chip-space"
-                      label={industry}
-                      color="secondary"
-                      size="small"
-                       />
-                    )
-                  })}
+                  <div className="location info-padding">
+                    <LocationOnIcon className="icon"/>
+                    <div className="text"> {item.to.displayLocation}</div>
                   </div>
-                <div className="interests">
-                {item.to.interests.map((interest, i) => {
-                    return (
-                      <Chip 
-                      key={`interest-chip-${i}`}
-                      className="chip-space"
-                      label={interest}
-                      size="small"
-                       />
-                    )
-                  })}
-                  
-                  </div>
-            </div>
-
+                  <div className="backgrounds info-padding">
+                    {item.to.backgrounds.map((background, i) => {
+                      return (
+                        <Chip 
+                        key={`background-chip-${i}`}
+                        className="chip-space"
+                        label={background}
+                        color="primary"
+                        size="small"
+                        />
+                      )
+                    })}
+                    </div>
+                  <div className="industries info-padding">
+                  {item.to.industries.map((industry, i) => {
+                      return (
+                        <Chip 
+                        key={`industry-chip-${i}`}
+                        className="chip-space"
+                        label={industry}
+                        color="secondary"
+                        size="small"
+                        />
+                      )
+                    })}
+                    </div>
+                  <div className="interests info-padding">
+                  {item.to.interests.map((interest, i) => {
+                      return (
+                        <Chip 
+                        key={`interest-chip-${i}`}
+                        className="chip-space"
+                        label={interest}
+                        size="small"
+                        />
+                      )
+                    })}
+                    
+                    </div>
+              </div>
+              <FormatQuoteIcon
+               color="primary"
+               size="large"
+               />
+              <div className="desc">
+                {item.to.aboutMe}
+              </div>
             </div>
            
             </SwiperSlide>
